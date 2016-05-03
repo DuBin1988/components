1d861fd63ec85742bdbed3cd13e04215d43cf5ec
何宁社 <524395609@qq.com>
2016-05-03 12:35:46 +0800

    busy组件初步完成

---
+++ examples/busy/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id='app'>
+    <busy :is-busy="isBusy">
+      <span partial>正常组件</span>
+    </busy>
+  </div>
+  <button @click='isBusy = true'>忙</busy>
+  <button @click='isBusy = false'>闲</busy>
+</template>
+
+<script>
+import Busy from '../../src/components/Busy'
+
+export default {
+  data () {
+    return {
+      isBusy: false
+    }
+  },
+  components: { Busy }
+}
+</script>
---
+++ src/components/Busy.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p v-if="isBusy">正在查询，请耐心等待！</p>
+    <div v-show="!isBusy">
+      <partial name='default'></partial>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isBusy: Boolean,
+    default: false
+  }
+}
+</script>
